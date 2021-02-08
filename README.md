f9ca150e7bc2fcaa12e3d9c9eaf4e2524abf4a02
ccev <42342921+ccev@users.noreply.github.com>
2021-02-08 01:24:19 +0100

    hello

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # faster-routes
- An alternate routecalc algorithm for MAD
+An alternate routecalc algorithm for MAD (made quick and dirty)
+
+### How to use
+
+This is a recommendation of how to use this script. It requires a bit of setup but the workflow in the end is pretty good. Note that I set up everything (except the MAD instance) on my own computer since everything will take a lot of power.
+
+- `cp config-example.json config.json`, fill the db part out
+- Set up a MAD instance to edit routes on (+ fill in the config and run):
+
+```
+git remote add mrjul https://github.com/MrJul/MAD
+git fetch --all
+git checkout --track mrjul/route-editor
+```
+
+- Clone `git clone https://github.com/Tabbomat/MADUtilities.git`, fill in its config (with values from your new instance)
+- In the faster-routes directory, create a new file `fence.txt`. This file should contain the geofence you want your route to be calculated it in. Copy the text from MADmin - Settings - Geofences - Edit
+- Run `cluster.py`
+- A file called `route.txt` will be created. Copy that to your MADUtilities directory. Then run `overwrite_route.py` there
+- Finished. For further improvement I highly recommend quickly viewing the route in your new MAD instance and make a few obvious edits
+
+If you want to create a new fence, just change `faster-routes/fence.txt`, run `cluster.py`, copy `route.txt` to MADutilities, run `overwrite_route.py`, go to MADmin and review the results.
+
+### How it works and what te config does
+
+- Go through each pair of spawns that are within `max_distance_between_spawns` meters of one another, find the middle point and save it together with the spawns in its `view_distance` meter range and if it covers more than `min_total_spawns_in_cluster` spawns. These will be all possible clusters we'll now check
+- Go through each spawnpoint, find all clusters containing this spawnpoint and save the one containing the most overall spawnpoints. This is how the main route will be built
+- Go through all routepoints and remove clusters that have added less than `min_added_spawns_in_cluster`. So if a cluster covers a total of 5 spawns, but those 5 spawns are already covered by other clusters, that cluster will be removed. If you raise this number, you can later manually add the missed spawns yourself.
+- The script will then try to "group" clusters
+
+### Possible improvements
+- Improve the way of generating possible routepoints (step 1) - currently a lot of power will be wasted on creating points in positions where there already is one close enough where it doesn't make a difference. Maybe add an option to save points from every ~5m² in the area. Would take ages to process but should greatly improve results
+- In the last step, try to recursively go through the route, delete a random routepoint and then try to cover it up with nearby points. Then do the same for those nearby points. Something like that. 
---
+++ cluster.py
@@ -0,0 +1,283 @@
+from pymysql import connect
+from math import sin, cos, sqrt, atan2, radians
+import math
+import random
+from geopy import distance as calc_distance
+import pickle
+
+def get_distance(point1, point2):
+    # approximate radius of earth in km
+    earth_radius = 6373.0
+
+    lat1 = math.radians(point1[0])
+    lon1 = math.radians(point1[1])
+    lat2 = math.radians(point2[0])
+    lon2 = math.radians(point2[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    angle = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    circ = 2 * math.atan2(math.sqrt(angle), math.sqrt(1 - angle))
+
+    distance = earth_radius * circ
+
+    return distance * 1000
+
+def get_middle_of_coord_list(list_of_coords):
+    if len(list_of_coords) == 1:
+        return list_of_coords[0]
+
+    coord_x = 0
+    coord_y = 0
+    coord_z = 0
+
+    for coord in list_of_coords:
+        # transform to radians...
+        lat_rad = math.radians(coord.lat)
+        lng_rad = math.radians(coord.lon)
+
+        coord_x += math.cos(lat_rad) * math.cos(lng_rad)
+        coord_y += math.cos(lat_rad) * math.sin(lng_rad)
+        coord_z += math.sin(lat_rad)
+
+    amount_of_coords = len(list_of_coords)
+    coord_x = coord_x / amount_of_coords
+    coord_y = coord_y / amount_of_coords
+    coord_z = coord_z / amount_of_coords
+    central_lng = math.atan2(coord_y, coord_x)
+    central_square_root = math.sqrt(coord_x * coord_x + coord_y * coord_y)
+    central_lat = math.atan2(coord_z, central_square_root)
+
+    return (math.degrees(central_lat), math.degrees(central_lng))
+
+import json
+with open("config.json", "r") as f:
+    config = json.load(f)
+max_distance = config["view_distance"]
+
+class Spawn:
+    def __init__(self, sid, lat, lon):
+        self.lat = lat
+        self.lon = lon
+        self.id = sid
+        self.done = False
+        self.clusters = []
+
+class Range:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+        self.spawns = []
+        self.double_spawns = []
+
+    def get_spawns(self):
+        return [spawn for spawn in self.spawns if not spawn.done]
+
+    def get_done_spawns(self):
+        return [spawn for spawn in self.spawns if spawn.done]
+
+    def get_spawn_id(self):
+        return [s.id for s in self.get_spawns()]
+
+    @property
+    def unique_spawns(self):
+        return [s for s in self.spawns if s.id not in [d.id for d in self.double_spawns]]
+
+def get_spawns():
+    connection = connect(
+        host=config["host"],
+        user=config["user"],
+        password=config["password"],
+        database=config["db_name"],
+        port=config["port"],
+        autocommit=True
+    )
+
+    with open("fence.txt", "r") as f:
+        area = f.read()
+
+    area = area.replace(",", " ").strip("\n")
+    area = area.split("\n")
+    area.append(area[0])
+    area = ",".join(area)
+    cursor = connection.cursor()
+    query = f"select spawnpoint, latitude, longitude from trs_spawn where ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON(({area}))'), point(latitude, longitude)) and eventid = 1 order by longitude, latitude"
+    cursor.execute(query)
+
+    r = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    spawns = []
+    for sid, (ssid, lat, lon) in enumerate(r):
+        spawns.append(Spawn(ssid, lat, lon))
+
+    return spawns
+
+spawns = get_spawns()
+
+def middle_points():
+    points = []
+    i = 1
+    total = f"{len(spawns)}"
+    for spawn1 in spawns:
+        for spawn2 in spawns:
+            distance = get_distance((spawn1.lat, spawn1.lon), (spawn2.lat, spawn2.lon))
+            if distance < config["max_distance_between_spawns"]:
+                middle = get_middle_of_coord_list([spawn1, spawn2])
+                routepoint = Range(middle[0], middle[1])
+                for spawn3 in spawns:
+                    if get_distance(middle, (spawn3.lat, spawn3.lon)) <= max_distance:
+                        routepoint.spawns.append(spawn3)
+                spawn_amount = len(routepoint.spawns)
+                if spawn_amount >= config["min_total_spawns_in_cluster"]:
+                    points.append(routepoint)
+        i += 1
+        print(f"Generating possible routepoints: {i}/{total}")
+    return points
+
+def point_points():
+    points = []
+    i = 1
+    total = len(spawns)
+    for spawn in spawns:
+        routepoint = Range(spawn.lat, spawn.lon)
+        for spawn3 in spawns:
+            if get_distance((spawn.lat, spawn.lon), (spawn3.lat, spawn3.lon)) <= max_distance:
+                routepoint.spawns.append(spawn3)
+        spawn_amount = len(routepoint.spawns)
+        if spawn_amount > 0:
+            print(f"{i}/{total}: {spawn_amount}")
+            points.append(routepoint)
+        i += 1
+    return points
+
+"""try:
+    with open("points.pickle", "rb") as handle:
+        spawns, points = pickle.load(handle)
+except:
+    points = middle_points()
+    to_save = (spawns, points)
+
+    with open("points.pickle", "wb") as handle:
+        pickle.dump(to_save, handle)"""
+
+points = middle_points()
+
+
+def cluster_v1(spawns, final=[]):
+    total = len(spawns)
+    final_len = 0
+    for i, spawn in enumerate(spawns, start=1):
+        if spawn.done:
+            continue
+        circles = []
+        for point in points:
+            if spawn.id in point.get_spawn_id():
+                circles.append(point)
+        
+        circle = max(circles, key=lambda point: (len(point.get_spawns())))
+        for circlespawn in circle.get_spawns():
+            circlespawn.done = True
+        final_len += 1
+
+        final.append(circle)
+        print(f"Generating route: {i}/{total} (length: {final_len})")
+    return final
+
+"""try:
+    with open("route.pickle", "rb") as handle:
+        final = pickle.load(handle)
+except:
+    final = cluster_v1(spawns)
+
+    with open("route.pickle", "wb") as handle:
+        pickle.dump(final, handle)"""
+final = cluster_v1(spawns)
+
+def check_doubles(cluster_list):
+    for cluster in cluster_list:
+        cluster_ids = [s.id for s in cluster.spawns]
+        for cluster2 in cluster_list:
+            if cluster2 == cluster:
+                continue
+            for spawn in cluster2.spawns:
+                if spawn.id in cluster_ids:
+                    if spawn.id not in [s.id for s in cluster2.double_spawns]:
+                        cluster2.double_spawns.append(spawn)
+    return cluster_list
+
+def check_final_route(final):
+    total_removed = 0
+    
+    for cluster in final:
+        if len(cluster.spawns) - len(cluster.double_spawns) < config["min_added_spawns_in_cluster"]:
+            recheck_clusters = []
+            for spawn in cluster.spawns:
+                recheck_clusters += spawn.clusters
+
+            final.remove(cluster)
+            total_removed += 1
+            check_doubles(recheck_clusters)
+            print("Removed one point from route that didn't add enough spawns")
+    if total_removed > 0:
+        final = check_final_route(final)
+    return final
+
+def edit_route(smallest, route):
+    for spawn in smallest.spawns:
+        for point in points:
+            if not spawn.id in [s.id for s in point.spawns]:
+                continue
+            if point == smallest:
+                continue
+
+            other_clusters = []
+            for spawn2 in point.spawns:
+                for cluster in spawn2.clusters:
+                    if cluster != smallest and cluster not in other_clusters and cluster in route:
+                        other_clusters.append(cluster)
+
+            if len(other_clusters) == 0:
+                continue
+
+            for cluster in other_clusters:
+                all_spawns = smallest.unique_spawns + cluster.unique_spawns
+
+                if [1 for s in all_spawns if s.id not in [s.id for s in point.spawns]]:
+                    continue
+
+                print("Optimize: Merged 2 routepoints")
+                route.remove(cluster)
+                route.remove(smallest)
+                route.append(point)
+                return
+
+for circle in final:
+    for spawn in circle.spawns:
+        spawn.clusters.append(circle)
+
+check_doubles(final)
+final = check_final_route(final)
+
+final_len = len(final)
+for i in range(final_len):
+    try:
+        smallest = final[i]
+    except:
+        break
+    edit_route(smallest, final)
+
+final = check_final_route(final)
+
+
+route = ""
+for circle in final:
+    route += f"{circle.lat},{circle.lon}\n"
+
+print(f"Wrote route to route.txt. Total length: {len(final)}")
+
+with open("route.txt", "w+") as f:
+    f.write(route)
